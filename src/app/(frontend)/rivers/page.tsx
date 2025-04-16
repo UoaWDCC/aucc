de417dac234f4498017888fea66e278b8165a130
Franck Chen <119908908+f13322@users.noreply.github.com>
2025-04-16 10:55:17 +0800

    Added routes to other pages (#32)
    * Added routes to other pages
    
    Added routes to the following pages:
    - about page:  /about
    - event page: /events
    - specific event page: /event/[eventName]
    - rivers page: /rivers
    - specific river page: /rivers/[riverName]
    - trip reports page: /trips
    - specific trip report page: /trips/[tripName]
    - merch page: /merch
    - gallery page: /gallery
    - gear hire page: /gear
    - resources page: /resources
    
    * Pruned unnecessary sections
    
    * removed unnecessary css and imports
    
    * fix some typos

---
+++ src/app/(frontend)/rivers/page.tsx
@@ -0,0 +1,13 @@
+export default async function RiversPage() {
+  return (
+    <div>
+      <div>
+        <h1>Welcome to the rivers page.</h1>
+        <p>Update this page by editing</p>
+        <a>
+          <code>app/(frontend)/rivers/page.tsx</code>
+        </a>
+      </div>
+    </div>
+  )
+}
