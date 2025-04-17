5a2b9ca0d6caab96ad48b54c5edee2342147980b
f13322 <119908908+f13322@users.noreply.github.com>
2025-04-18 02:13:27 +1200

    add collection for events

---
+++ src/collections/events.ts
@@ -0,0 +1,65 @@
+import {
+  defaultEditorFeatures,
+  lexicalEditor,
+} from '@payloadcms/richtext-lexical'
+import type { CollectionConfig } from 'payload'
+
+import { anyone } from '@/access/anyone'
+import { authenticated } from '@/access/authenticated'
+
+export const Events: CollectionConfig = {
+  slug: 'events',
+  access: {
+    create: authenticated,
+    read: anyone,
+    update: authenticated,
+    delete: authenticated,
+  },
+  fields: [
+    {
+      name: 'title',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'status',
+      type: 'select',
+      options: ['draft', 'published', 'archived'],
+      defaultValue: 'draft',
+      required: true,
+    },
+    {
+      name: 'startTime',
+      type: 'date',
+      admin: {
+        date: {
+          pickerAppearance: 'dayAndTime',
+        },
+      },
+      required: true,
+    },
+    {
+      name: 'endTime',
+      type: 'date',
+      admin: {
+        date: {
+          pickerAppearance: 'dayAndTime',
+        },
+      },
+    },
+    {
+      name: 'location',
+      type: 'text',
+    },
+    {
+      name: 'description',
+      type: 'richText',
+    },
+    {
+      name: 'featuredImage',
+      type: 'relationship',
+      hasMany: false,
+      relationTo: 'media',
+    },
+  ],
+}
