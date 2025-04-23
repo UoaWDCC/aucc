0df26330e4b7b48b24346e76313b9d8acd4cb4f5
dyzhuu <davidyzhu1973@gmail.com>
2025-04-23 16:22:40 +1200

    Enhance rivers functionality and structure
    - Added loading state for rivers page with a fallback component.
    - Implemented a grid layout for displaying river cards.
    - Introduced slug generation for rivers and trip reports collections.
    - Updated configuration to support remote image patterns.
    - Refactored trip reports collection import path.
    - Removed unused route file for better code cleanliness.
    - Added utility functions for fetching rivers data from the payload.

--- src/app/(frontend)/rivers/page.tsx
+++ src/app/(frontend)/rivers/page.tsx
@@ -1,12 +1,28 @@
+import { Suspense } from 'react'
+
+import { getRivers } from '@/queries/rivers'
+import { RiversGrid } from './_components/RiversGrid'
+import { RiversGridFallback } from './_components/RiversGridFallback'
+
 export default async function RiversPage() {
+  const { rivers } = await getRivers({ limit: 10 })
+
+  // Artificial delay to simulate a slow network
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+
   return (
-    <div>
+    <div className="p-4">
       <div>
-        <h1>Welcome to the rivers page.</h1>
-        <p>Update this page by editing</p>
-        <a>
-          <code>app/(frontend)/rivers/page.tsx</code>
-        </a>
+        <h1 className="text-2xl font-bold">Rivers</h1>
+        <h2 className="text-gray-600">
+          A list of all rivers available for paddling.
+        </h2>
+      </div>
+
+      <div className="mt-4">
+        <Suspense fallback={<RiversGridFallback />}>
+          <RiversGrid rivers={rivers} />
+        </Suspense>
       </div>
     </div>
   )
---
+++ src/queries/rivers.ts
@@ -0,0 +1,50 @@
+import { getPayloadClient } from '@/lib/payload'
+import type { River } from '@/payload-types'
+
+/**
+ * Get all rivers
+ * @param page - The page number to get
+ * @param limit - The number of rivers to get per page
+ * @returns The rivers and pagination information
+ */
+export async function getRivers({ page = 1, limit = 10, sort = 'grade' } = {}) {
+  const payload = await getPayloadClient()
+
+  const { docs, hasNextPage, nextPage, totalDocs } = await payload.find({
+    collection: 'rivers',
+    page,
+    limit,
+    sort,
+  })
+
+  return {
+    rivers: docs as River[],
+    hasNextPage,
+    nextPage,
+    totalDocs,
+  }
+}
+
+/**
+ * Get a river by its ID
+ * @param id - The ID of the river to get
+ * @returns The river
+ */
+export async function getRiverBySlug(slug: string): Promise<River | null> {
+  const payload = await getPayloadClient()
+
+  const river = await payload.find({
+    collection: 'rivers',
+    where: {
+      slug: {
+        equals: slug,
+      },
+    },
+  })
+
+  if (river.docs.length === 0) {
+    return null
+  }
+
+  return river.docs[0] as River
+}
