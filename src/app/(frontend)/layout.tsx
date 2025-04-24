d75495654ea586f569acd568a07cadccd78f6d8c
BlackPri0r <160561469+BlackPri0r@users.noreply.github.com>
2025-04-24 16:58:59 +1200

    fix: body tag in layout

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -15,7 +15,7 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
 
   return (
     <html lang="en">
-      <body>
+      <body className="min-h-dvh">
         <Header />
         <main>{children}</main>
         <Footer />
