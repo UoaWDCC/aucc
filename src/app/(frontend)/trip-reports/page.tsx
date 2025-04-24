48e6e5ba8e56159e3f32407d202d7348b41ce028
henri <henrichemla@outlook.com>
2025-04-24 14:34:36 +1200

    pushing hello there

--- src/app/(frontend)/trip-reports/page.tsx
+++ src/app/(frontend)/trip-reports/page.tsx
@@ -4,6 +4,7 @@ export default async function TripReportsPage() {
       <div>
         <h1>Welcome to the trip reports page.</h1>
         <p>Update this page by editing</p>
+        <p>Hello there</p>
         <a>
           <code>app/(frontend)/trips/page.tsx</code>
         </a>
