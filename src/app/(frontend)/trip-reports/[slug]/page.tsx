d715feead479a8cd7027d8dbcd426642740db6f3
Warri0r2005 <dom.pietersen.1@gmail.com>
2025-04-28 16:45:47 +1200

    Final styling and added author

--- src/app/(frontend)/trip-reports/[slug]/page.tsx
+++ src/app/(frontend)/trip-reports/[slug]/page.tsx
@@ -1,5 +1,4 @@
 import { RichText } from '@payloadcms/richtext-lexical/react'
-import { getPayload } from 'payload'
 
 import { getTripReportBySlug } from '@/queries/trip-reports'
 
@@ -27,11 +26,19 @@ export default async function SpecificTripReportPage({
     : ''
 
   return (
-    <div className="m-20 flex h-full w-[90vw] flex-col bg-gray-100 p-15 pt-0">
+    <div className="m-25 mt-0 flex h-full w-auto flex-col bg-gray-100 p-15 pt-0">
       <div>
         <div>
-          <h1 className="p-10 pb-5 text-4xl font-bold">{tripReport.title}</h1>
-          <p className="p-10 pt-0 text-2xl">Date: {date}</p>
+          <h1 className="p-10 pb-5 pl-0 text-4xl font-bold">
+            {tripReport.title}
+          </h1>
+          <div>
+            <p className="text-2xl">Date: {date}</p>
+            <p className="pt-2 text-2xl">
+              Authors:{' '}
+              {tripReport.author.map((author: any) => author.name).join(', ')}
+            </p>
+          </div>
         </div>
         <div className="w-full bg-white p-5">
           {tripReport.content && <RichText data={tripReport.content} />}
