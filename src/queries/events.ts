66674d30f652cddd5a8af260b908ce505a0447b7
avalee0215 <161207991+avalee0215@users.noreply.github.com>
2025-05-08 17:56:35 +1200

    Create a basic events page draft (#45)
    * Tidy up config and README (#6)
    
    * tidies up some config
    
    * add lint-staged & precommit hooks
    
    * tidies up README
    
    * tidies up issue template
    
    * Added my name to the README (#18)
    
    * Henri adding his name to the readme (#19)
    
    * add markdown to vscode settings.json
    
    * Create a basic events page draft
    
    * Fix the page.tsx: Wrap useSearchParams in Suspense to fix build error
    
    * Follow the format of the river page. Satisfied all the requirements in the ticket.
    
    * Fixed the error from events.test.ts based on the github
    
    * Reset [slug]/page.tsx file
    
    * Edit queries/events.ts and events/test/ts getAllEvents()
    
    * trip-report.ts has a conflict with event.ts as the slug field is repeated. Fixed that part
    
    * Used main branch payload-types.ts as the slug error seems to be the problem of the payload-types.ts, not events.ts

--- src/queries/events.ts
+++ src/queries/events.ts
@@ -1,6 +1,33 @@
 import { getPayloadClient } from '@/lib/payload'
 import type { Event } from '@/payload-types'
 
+// Get all events
+export async function getAllEvents({
+  page = 1,
+  limit = 10,
+  sort = '-startTime',
+}: {
+  page?: number
+  limit?: number
+  sort?: string
+} = {}) {
+  const payload = await getPayloadClient()
+
+  const { docs, hasNextPage, nextPage, totalDocs } = await payload.find({
+    collection: 'events',
+    page,
+    limit,
+    sort,
+  })
+
+  return {
+    events: docs as Event[],
+    hasNextPage,
+    nextPage,
+    totalDocs,
+  }
+}
+
 /**
  * Get a event by its ID
  * @param id - The ID of the event to get
@@ -19,3 +46,4 @@ export async function getEventBySlug(slug: string): Promise<Event | null> {
     return null
   }
 }
+
