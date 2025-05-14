a17d5ea012a8f44e77d8b00bf1c8aa2ce9e3d4ac
Mai Pham <phamngocmei0912@gmail.com>
2025-05-14 15:52:33 +1200

    add changes to river page

--- src/app/(frontend)/rivers/[slug]/page.tsx
+++ src/app/(frontend)/rivers/[slug]/page.tsx
@@ -1,18 +1,31 @@
-export default async function RiverPage({
+import { Suspense } from 'react'
+import { notFound } from 'next/navigation'
+
+import { getRiverBySlug } from '@/queries/rivers'
+import { RiverPage } from './_component/RiverPage'
+import { RiverPageFallback } from './_component/RiverPageFallback'
+
+export default async function SpecificRiverPage({
   params,
 }: {
   params: Promise<{ slug: string }>
 }) {
   const { slug } = await params
+  const river = await getRiverBySlug(slug)
+
+  console.log(river)
+
+  if (!river) {
+    notFound()
+  }
 
   return (
     <div>
       <div>
         <h1>Welcome to the specific river page, current trip is {slug}</h1>
-        <p>Update this page by editing</p>
-        <a>
-          <code>app/(frontend)/rivers/[slugs]/page.tsx</code>
-        </a>
+        <Suspense fallback={<RiverPageFallback />}>
+          <RiverPage river={river} />
+        </Suspense>
       </div>
     </div>
   )
