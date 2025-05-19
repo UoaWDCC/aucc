2a3293acba141de3b22dd7aee4c1eb09e9e35041
mei0912 <phamngocmei0912@gmail.com>
2025-05-19 16:43:25 +1200

    mei0912 #52 Create single river page  (#70)
    * add changes to river page
    
    * change in page.tsx

--- src/app/(frontend)/rivers/[slug]/page.tsx
+++ src/app/(frontend)/rivers/[slug]/page.tsx
@@ -1,18 +1,25 @@
-export default async function RiverPage({
+import { notFound } from 'next/navigation'
+
+import { getRiverBySlug } from '@/queries/rivers'
+import { RiverPage } from './_component/RiverPage'
+
+export default async function SpecificRiverPage({
   params,
 }: {
   params: Promise<{ slug: string }>
 }) {
   const { slug } = await params
+  const river = await getRiverBySlug(slug)
+
+  if (!river) {
+    notFound()
+  }
 
   return (
     <div>
       <div>
         <h1>Welcome to the specific river page, current trip is {slug}</h1>
-        <p>Update this page by editing</p>
-        <a>
-          <code>app/(frontend)/rivers/[slugs]/page.tsx</code>
-        </a>
+        <RiverPage river={river} />
       </div>
     </div>
   )
