4d7f14f63ce5bf55072088e5cd49c0e7161f0f1f
f13322 <119908908+f13322@users.noreply.github.com>
2025-05-24 17:17:12 +1200

    add comments

--- src/lib/utils/revalidation.ts
+++ src/lib/utils/revalidation.ts
@@ -1,7 +1,22 @@
 import { revalidateTag } from 'next/cache'
 
+/**
+ @typedef CacheTag Custom type definined by keys of tagRelatinos
+ */
 export type CacheTag = keyof typeof tagRelations
 
+/** 
+ * Dictionary representing the relationship between collections
+ * 
+ * Update when adding new collections or new relationships are added
+ * 
+ * Key - The tag for collection
+ * 
+ * Value - List of tags for collections that it is relatedTo
+ * 
+ @constant
+ @type {readonly CacheTag: readonly CacheTag[]}
+ */
 const tagRelations = {
   events: ['media'],
   execs: ['media'],
@@ -10,6 +25,19 @@ const tagRelations = {
   tripReports: ['media'],
 } as const
 
+/** 
+ * Dictionary of cacheTags with list of related tags and funciton to revalidate tag
+ * 
+ * Key - The CacheTags for the collection
+ * 
+ * Value - Object containing list of related CacheTags and function to revalidate collection
+ * 
+ * Update when adding new collections or new relationships are added
+ @constant
+ @type {CacheTag: {CacheTag, { relatedTags, revalidate }}}
+ @property { CacheTag[] } relatedTags - All CacheTags related to collection
+ @property { (CacheTag) => Void} revalidate - Function to revalidate collection
+ */
 export const cacheTags: Record<
   CacheTag,
   {
