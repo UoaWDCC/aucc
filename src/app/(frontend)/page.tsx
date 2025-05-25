e3c05fd3fce2195740ab1fabe8f953b0302478bb
f13322 <119908908+f13322@users.noreply.github.com>
2025-05-26 03:34:56 +1200

    test

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -5,12 +5,19 @@ import Image from 'next/image'
 import { getPayload } from 'payload'
 
 import { RecentContent } from '@/components/RecentContent'
+import { WelcomeSectionBackground } from '@/components/WelcomeSection'
 import config from '@/payload.config'
-
 import HeroSection from './_components/sections/HomePage/Hero/HeroSection'
 
 import './styles.css'
 
+function TEST() {
+  return (
+    <div>
+      <div className="z-20 w-full bg-red-500">200</div>
+    </div>
+  )
+}
 export default async function HomePage() {
   const headers = await getHeaders()
   const payloadConfig = await config
@@ -22,10 +29,13 @@ export default async function HomePage() {
   return (
     <div className="home">
       <div className="content">
-        
+        <h1>Welcome to AUCC</h1>
         <Suspense fallback={<p>Loading content...</p>}>
-          <HeroSection />
-          <RecentContent /> 
+          <RecentContent />
+          <div className="h-[15vh] w-full">1</div>
+          <WelcomeSectionBackground>
+            <TEST></TEST>
+          </WelcomeSectionBackground>
         </Suspense>
 
         <div className="footer">
