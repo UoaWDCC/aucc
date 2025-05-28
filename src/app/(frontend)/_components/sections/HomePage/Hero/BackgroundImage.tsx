5de65bfe0a8a7b39d79e51278e7bcceff67afad2
Warri0r2005 <dom.pietersen.1@gmail.com>
2025-05-28 18:14:49 +1200

    Updated sizing and spacing to match figma

--- src/app/(frontend)/_components/sections/HomePage/Hero/BackgroundImage.tsx
+++ src/app/(frontend)/_components/sections/HomePage/Hero/BackgroundImage.tsx
@@ -7,14 +7,15 @@ interface BackgroundImageProps {
 
 export function BackgroundImage({ src, alt }: BackgroundImageProps) {
   return (
-    <div className="absolute inset-0 z-0 h-full w-full">
+    <div className="absolute inset-0 z-0 h-full w-full overflow-hidden">
       <Image
         src={src}
         alt={alt}
         fill
         className="absolute inset-0 z-0 h-full w-full object-cover brightness-90"
+        style={{ transform: 'scale(1.2) translateX(100px)' }}
       />
-      <div className="absolute inset-0 z-1 h-full w-full bg-gradient-to-t from-black/40 to-white/20"></div>
+      <div className="absolute inset-0 z-1 h-full w-full bg-gradient-to-t from-black/60 to-white/15 opacity-90"></div>
     </div>
   )
 }
--- src/app/(frontend)/_components/sections/HomePage/Hero/HeroContent.tsx
+++ src/app/(frontend)/_components/sections/HomePage/Hero/HeroContent.tsx
@@ -4,7 +4,7 @@ import { ScrollCue } from './ScrollCue'
 export function HeroContent() {
   return (
     <div className="text-cream relative z-20 inline-block w-auto px-4 text-center">
-      <h1 className="mb-2 text-2xl font-extrabold tracking-[.2em] md:text-4xl">
+      <h1 className="mb-0 pb-0 text-2xl font-extrabold tracking-[.1em] [word-spacing:20px] md:text-5xl">
         WELCOME TO
         <br />
         <span className="text-6xl font-black drop-shadow-xl md:text-[11rem]">
@@ -12,13 +12,12 @@ export function HeroContent() {
         </span>
       </h1>
       <div className="flex w-auto flex-row">
-        <p className="mt-4 inline-block h-auto w-90 text-left text-base italic md:text-xl">
-          Whitewater kayaking &amp; rafting in New Zealand’s most beautiful
-          places.
+        <p className="mt-4 inline-block h-auto w-90 text-left text-base italic md:text-[20px]">
+          Whitewater kayaking &amp; rafting in <br />
+          New Zealand’s most beautiful places
         </p>
         <CTAButton />
       </div>
-      <ScrollCue /> {/* Replace with styled cue later */}
     </div>
   )
 }
