eba39650155a936e2e0665d4fed6534ff2fd9820
dyzhuu <davidyzhu1973@gmail.com>
2025-05-29 12:46:39 +1200

    fix: update gradient direction in TripReportsSection component

--- src/app/(frontend)/_components/home/trip-reports/TripReportsSection.tsx
+++ src/app/(frontend)/_components/home/trip-reports/TripReportsSection.tsx
@@ -1,6 +1,6 @@
 export function TripReportsSection() {
   return (
-    <div className="min-h-96 bg-[conic-gradient(from_-90deg,#424B44_0deg,#78ACAD_55.3116deg,#BED66D_204.231deg,#3E433D_355.905deg,#424B44_360deg)]">
+    <div className="min-h-96 bg-[conic-gradient(from_-100deg,#424B44_0deg,#78ACAD_55.3116deg,#BED66D_204.231deg,#3E433D_355.905deg,#424B44_360deg)]">
       Trip Reports Section
     </div>
   )
