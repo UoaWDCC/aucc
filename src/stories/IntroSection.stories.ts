7ed17ae40688adbb05660d8b290f7ea9bb826a56
f13322 <119908908+f13322@users.noreply.github.com>
2025-05-31 23:00:49 +1200

    fix export for intro section story

--- src/stories/IntroSection.stories.ts
+++ src/stories/IntroSection.stories.ts
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import { fn } from '@storybook/test'
 
-import IntroSection from '@/app/(frontend)/_components/home/intro/IntroSection'
+import { IntroSection } from '@/app/(frontend)/_components/home/intro/IntroSection'
 
 const meta = {
   title: 'WelcomeSection',
