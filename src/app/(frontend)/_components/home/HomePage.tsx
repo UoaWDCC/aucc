5fa739767ef3a5d778a48dd4e9c4831b38b7d8ee
henri <henrichemla@outlook.com>
2025-06-02 22:22:44 +1200

    putting all the code for the cards and the styling in this file

--- src/app/(frontend)/_components/home/HomePage.tsx
+++ src/app/(frontend)/_components/home/HomePage.tsx
@@ -1,3 +1,4 @@
+import type { TripReport } from '@/payload-types'
 import { ContactUsSection } from './contact-us/ContactUsSection'
 import { GallerySection } from './gallery/GallerySection'
 import { HeroSection } from './hero/HeroSection'
@@ -5,14 +6,18 @@ import { IntroSection } from './intro/IntroSection'
 import { NextAdventureSection } from './next-adventure/NextAdventureSection'
 import { TripReportsSection } from './trip-reports/TripReportsSection'
 
-export function HomePage() {
+interface HomePageProps {
+  latestReports: TripReport[]
+}
+
+export function HomePage({ latestReports }: HomePageProps) {
   return (
     <>
       <HeroSection />
       <IntroSection />
       <NextAdventureSection />
       <GallerySection />
-      <TripReportsSection />
+      <TripReportsSection tripReports={latestReports} />
       <ContactUsSection />
     </>
   )
