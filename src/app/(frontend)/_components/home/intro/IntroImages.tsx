bedb028b4c7f8562446081d9790d978ddc6082a4
Franck Chen <119908908+f13322@users.noreply.github.com>
2025-06-02 23:10:35 +1200

    Create Welcome Section (#89)
    * test
    
    * test
    
    * finish mobile version
    
    * break welcome section into parts
    
    * Rename Welcomesection.tsx to WelcomeSection.tsx
    
    * move WelcomeContent into WelcomeSection
    
    * test
    
    * finish mobile version
    
    * test
    
    * break welcome section into parts
    
    * Rename Welcomesection.tsx to WelcomeSection.tsx
    
    * move WelcomeContent into WelcomeSection
    
    * `pnpm dedupe`
    
    * remove stories
    
    * update md css
    
    * test
    
    * fix lockfile :)
    
    * rename welcome to intro
    
    * add scaling to images and text location
    
    * remove background
    
    * move componenst to right place
    
    * fix export for intro section story
    
    * kill storybook tests
    
    ---------
    
    Co-authored-by: dyzhuu <davidyzhu1973@gmail.com>

---
+++ src/app/(frontend)/_components/home/intro/IntroImages.tsx
@@ -0,0 +1,45 @@
+import Image from 'next/image'
+import { object } from 'zod'
+
+import { env } from '@/lib/env'
+
+export function IntroImages() {
+  return (
+    <>
+      <div className="relative mt-31 mr-auto ml-auto h-auto max-w-140">
+        <div className="absolute -left-4 aspect-[54/44] w-[58%] overflow-hidden rounded-2xl">
+          <Image
+            src={`${env.NEXT_PUBLIC_CF_URL}/static/homepage/welcome-01.v1.webp`}
+            fill
+            className="object-cover"
+            alt="Intro picture 1"
+          />
+        </div>
+        <div className="absolute right-[32%] z-2 mt-[50%] aspect-[20/25] w-[22%] overflow-hidden rounded-2xl">
+          <Image
+            src={`${env.NEXT_PUBLIC_CF_URL}/static/homepage/welcome-02.v1.webp`}
+            fill
+            className="object-cover"
+            alt="Intro picture 2"
+          />
+        </div>
+        <div className="absolute right-4 z-1 mt-[21%] aspect-[28/43] w-[30%] overflow-hidden rounded-2xl">
+          <Image
+            src={`${env.NEXT_PUBLIC_CF_URL}/static/homepage/welcome-03.v1.webp`}
+            fill
+            className="object-cover"
+            alt="Intro picture 3"
+          />
+        </div>
+      </div>
+      <div className="absolute top-25 -left-9 h-[calc(0.21*65vw)] w-[65%] min-w-75 origin-top-right rotate-7 overflow-hidden">
+        <Image
+          src={`${env.NEXT_PUBLIC_CF_URL}/static/homepage/paddle.v1.webp`}
+          fill
+          className="object-cover"
+          alt="Paddle"
+        />
+      </div>
+    </>
+  )
+}
