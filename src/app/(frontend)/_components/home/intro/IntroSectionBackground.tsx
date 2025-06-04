227d72347d7185e1dd16eabbe0d860b40cf7f15e
f13322 <franck13322@gmail.com>
2025-06-04 18:14:20 +1200

    Update IntroSectionBackground.tsx

--- src/app/(frontend)/_components/home/intro/IntroSectionBackground.tsx
+++ src/app/(frontend)/_components/home/intro/IntroSectionBackground.tsx
@@ -7,5 +7,10 @@ interface IntroSectionBackGroundProps {
 export function IntroSectionBackground({
   children,
 }: IntroSectionBackGroundProps) {
-  return <div className="relative inset-0 w-full">{children}</div>
+  return (
+    <div className="relative inset-0 w-full">
+      <div className="bg-water absolute bottom-0 h-[60vw] max-h-83 w-full"></div>
+      {children}
+    </div>
+  )
 }
