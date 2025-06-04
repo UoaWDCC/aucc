b5665ac55b40cb68199d332b74fbe54c050c1c55
Warri0r2005 <dom.pietersen.1@gmail.com>
2025-06-04 15:21:10 +1200

    Remove translations for background image

--- src/app/(frontend)/_components/sections/HomePage/Hero/BackgroundImage.tsx
+++ src/app/(frontend)/_components/sections/HomePage/Hero/BackgroundImage.tsx
@@ -13,7 +13,7 @@ export function BackgroundImage({ alt }: BackgroundImageProps) {
         src={`${env.NEXT_PUBLIC_CF_URL}/static/homepage/hero.v1.webp`}
         alt={alt}
         fill
-        className="absolute inset-0 size-full translate-y-[-60px] scale-130 object-cover brightness-90 md:translate-x-25 md:scale-120"
+        className="absolute inset-0 size-full object-cover brightness-90"
       />
       <div className="absolute inset-0 z-1 h-full w-full bg-gradient-to-t from-black/60 to-white/15 opacity-90" />
     </div>
