a1a5eb96b6417a099e4f530d91f196052f9c9dd2
Warri0r2005 <dom.pietersen.1@gmail.com>
2025-06-04 15:19:28 +1200

    Fix background image url

--- src/app/(frontend)/_components/sections/HomePage/Hero/BackgroundImage.tsx
+++ src/app/(frontend)/_components/sections/HomePage/Hero/BackgroundImage.tsx
@@ -1,22 +1,20 @@
 import Image from 'next/image'
 
+import { env } from '@/lib/env'
+
 interface BackgroundImageProps {
   alt: string
 }
 
 export function BackgroundImage({ alt }: BackgroundImageProps) {
-  const baseUrl = process.env.NEXT_PUBLIC_CF_URL
-
   return (
     <div className="absolute inset-0 z-0 h-full w-full overflow-hidden">
-      {baseUrl && (
-        <Image
-          src={`${baseUrl}/static/homepage/hero.v1.webp`}
-          alt={alt}
-          fill
-          className="absolute inset-0 size-full translate-y-[-60px] scale-130 object-cover brightness-90 md:translate-x-25 md:scale-120"
-        />
-      )}
+      <Image
+        src={`${env.NEXT_PUBLIC_CF_URL}/static/homepage/hero.v1.webp`}
+        alt={alt}
+        fill
+        className="absolute inset-0 size-full translate-y-[-60px] scale-130 object-cover brightness-90 md:translate-x-25 md:scale-120"
+      />
       <div className="absolute inset-0 z-1 h-full w-full bg-gradient-to-t from-black/60 to-white/15 opacity-90" />
     </div>
   )
--- src/lib/env.ts
+++ src/lib/env.ts
@@ -10,11 +10,15 @@ export const env = createEnv({
     S3_ACCESS_KEY_ID: z.string().min(1),
     S3_SECRET_ACCESS_KEY: z.string().min(1),
     S3_REGION: z.string().min(1),
-    S3_CF_PUBLIC_ENDPOINT: z.string().min(1),
     API_KEY: z.string().min(1),
+  },
+  client: {
+    NEXT_PUBLIC_CF_URL: z.string().min(1),
   },
   // this can be used in Next.js^13.4.4 and above,
   // client side runtime environment variables still need to be manually destructured
-  experimental__runtimeEnv: process.env,
+  experimental__runtimeEnv: {
+    NEXT_PUBLIC_CF_URL: process.env.NEXT_PUBLIC_CF_URL,
+  },
   skipValidation: process.env.SKIP_ENV_VALIDATION === 'true',
 })
