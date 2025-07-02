ac46168b42dfdb110e64e488df7507ad3458fe1e
Dom Pietersen <82022984+Warri0r2005@users.noreply.github.com>
2025-07-03 09:19:16 +1200

    Create Hero Section (#88)
    * Updated Stylings and changed CTA button
    
    * Update width of sub text
    
    * Make button and words on same level
    
    * Updated sizing and spacing to match figma
    
    * Updated mobile to fit figma
    
    * Hard code image and fix up styling to fit figma.
    Fix button hitbox
    
    * Fix background image url
    
    * Remove translations for background image
    
    * Updated button, text sizes for large devies and fixed spacing to figma. Updated background gradient
    
    * updated fonts and spacing for each screen size
    
    * Update some spacings to flex gap
    
    * Added scroll cue text and svg with styling
    
    * Updated sub text and button div
    
    * adjust background image gradient
    
    ---------
    
    Co-authored-by: dyzhuu <davidyzhu1973@gmail.com>

--- src/app/(frontend)/_components/home/hero/BackgroundImage.tsx
+++ src/app/(frontend)/_components/home/hero/BackgroundImage.tsx
@@ -1,17 +1,21 @@
 import Image from 'next/image'
 
+import { env } from '@/lib/env'
+
 interface BackgroundImageProps {
-  src: string
   alt: string
 }
 
-export function BackgroundImage({ src, alt }: BackgroundImageProps) {
+export function BackgroundImage({ alt }: BackgroundImageProps) {
   return (
-    <Image
-      src={src}
-      alt={alt}
-      fill
-      className="absolute inset-0 z-0 h-full w-full object-cover"
-    />
+    <div className="absolute inset-0 z-0 h-full w-full overflow-hidden">
+      <Image
+        src={`${env.NEXT_PUBLIC_CF_URL}/static/homepage/hero.v1.webp`}
+        alt={alt}
+        fill
+        className="absolute inset-0 size-full object-cover brightness-90"
+      />
+      <div className="absolute inset-0 z-1 h-full w-full bg-gradient-to-t from-black/60 to-white/15 opacity-80" />
+    </div>
   )
 }
--- src/app/(frontend)/_components/home/hero/HeroBottomCurve.tsx
+++ src/app/(frontend)/_components/home/hero/HeroBottomCurve.tsx
@@ -5,13 +5,15 @@ interface HeroBottomCurveProps {
 }
 export function HeroBottomCurve({ className }: HeroBottomCurveProps) {
   return (
-    <svg
-      viewBox="0 0 1280 110"
-      xmlns="http://www.w3.org/2000/svg"
-      className={cn('fill-cream h-14 w-full md:h-auto', className)}
-      preserveAspectRatio="none"
-    >
-      <path d="M2.70312 54.0514C463.155 225.655 945.947 -73.0313 1280 17.5045V110H0V53.0387L2.70312 54.0514Z" />
-    </svg>
+    <div className="absolute bottom-0 z-100 w-full translate-y-0.5">
+      <svg
+        viewBox="0 0 1280 110"
+        xmlns="http://www.w3.org/2000/svg"
+        className={cn('fill-cream h-14 w-full md:h-auto', className)}
+        preserveAspectRatio="none"
+      >
+        <path d="M2.70312 54.0514C463.155 225.655 945.947 -73.0313 1280 17.5045V110H0V53.0387L2.70312 54.0514Z" />
+      </svg>
+    </div>
   )
 }
