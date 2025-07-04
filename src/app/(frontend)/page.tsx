886be4ee3aaeb8e92a40b6b9b92bfc7b43db8998
dyzhuu <davidyzhu1973@gmail.com>
2025-07-04 23:49:07 +1200

    refactor: trip report components

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -2,12 +2,5 @@ import { getTripReports } from '@/queries/trip-reports'
 import { HomePage } from './_components/home/HomePage'
 
 export default async function Page() {
-  // This will get the latest trip reports for the trip report section
-  const { tripReports } = await getTripReports({
-    page: 1,
-    limit: 3,
-    sort: '-tripDate',
-  })
-
-  return <HomePage latestReports={tripReports} />
+  return <HomePage />
 }
