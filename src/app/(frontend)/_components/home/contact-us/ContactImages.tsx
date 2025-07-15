c082cb96f238cfa8bff39d785f6666ac6c11b38c
dyzhuu <davidyzhu1973@gmail.com>
2025-07-16 04:12:03 +1200

    refactor: image imports in contact and intro sections to use local assets

--- src/app/(frontend)/_components/home/contact-us/ContactImages.tsx
+++ src/app/(frontend)/_components/home/contact-us/ContactImages.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 
-import { env } from '@/lib/env'
+import lookingForUs01 from '@/assets/looking-for-us-01.webp'
+import lookingForUs02 from '@/assets/looking-for-us-02.webp'
+import lookingForUs03 from '@/assets/looking-for-us-03.webp'
 
 export function ContactImages() {
   return (
@@ -8,7 +10,7 @@ export function ContactImages() {
       {/* Image 1 */}
       <div className="absolute right-2 bottom-40 z-1 aspect-[2/3] w-40 overflow-hidden rounded-2xl md:right-4 md:bottom-55 md:w-44 lg:right-8 lg:bottom-70 lg:w-60">
         <Image
-          src={`${env.NEXT_PUBLIC_CF_URL}/static/homepage/looking-for-us-01.v1.webp`}
+          src={lookingForUs01}
           alt="Looking picture 1"
           fill
           className="object-cover"
@@ -18,7 +20,7 @@ export function ContactImages() {
       {/* Image 2 */}
       <div className="absolute right-26 bottom-28 z-2 aspect-[2/3] w-24 overflow-hidden rounded-2xl md:right-34 md:bottom-40 md:w-30 lg:right-55 lg:bottom-50 lg:w-36">
         <Image
-          src={`${env.NEXT_PUBLIC_CF_URL}/static/homepage/looking-for-us-02.v1.webp`}
+          src={lookingForUs02}
           alt="Looking picture 2"
           fill
           className="object-cover"
@@ -27,7 +29,7 @@ export function ContactImages() {
       {/* Image 3 */}
       <div className="absolute right-2 bottom-10 z-3 aspect-[4/3] w-20 overflow-hidden rounded-2xl md:right-4 md:w-30 lg:right-8 lg:w-40">
         <Image
-          src={`${env.NEXT_PUBLIC_CF_URL}/static/homepage/looking-for-us-03.v1.webp`}
+          src={lookingForUs03}
           alt="Looking picture 3"
           fill
           className="object-cover"
