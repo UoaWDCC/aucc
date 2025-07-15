955efb564012a830f8f9e661fec39b4433886a7a
BlackPri0r <lawrenceli630@gmail.com>
2025-07-16 03:18:10 +1200

    REFACTOR: Added tablet/desktop responsiveness

--- src/app/(frontend)/_components/home/next-adventure/NextAdventureButtons.tsx
+++ src/app/(frontend)/_components/home/next-adventure/NextAdventureButtons.tsx
@@ -6,12 +6,12 @@ type NextAdventureButtonProps = {
 
 export function NextAdventureButtons({ eventId }: NextAdventureButtonProps) {
   return (
-    <div className="flex w-full items-start gap-3.5">
-      <button className="text-abyss bg-algae cursor-pointer rounded-3xl px-5 py-2 text-xs">
+    <div className="flex w-full items-start gap-3.5 text-xs md:text-[6px] lg:text-[10px]">
+      <button className="text-abyss bg-algae font-unbounded cursor-pointer rounded-3xl px-5 py-2 md:px-4 md:py-1.5 lg:px-6 lg:py-3">
         Sign Up
       </button>
       <Link href={`/events/${eventId}`}>
-        <button className="text-algae cursor-pointer rounded-3xl border-2 px-3 py-1.5 text-xs">
+        <button className="text-algae font-unbounded cursor-pointer rounded-3xl border-2 px-3 py-1.5 md:px-2 md:py-1 lg:px-4 lg:py-2.5">
           Trip Details
         </button>
       </Link>
--- src/app/(frontend)/_components/home/next-adventure/NextAdventureCard.tsx
+++ src/app/(frontend)/_components/home/next-adventure/NextAdventureCard.tsx
@@ -13,35 +13,43 @@ interface EventPageProps {
 
 export function NextAdventureCard({ event }: EventPageProps) {
   return (
-    <div className="p-4 pt-0">
-      <div className="bg-cream/5 text-cream flex flex-col items-center gap-4 p-4">
-        {event.featuredImage &&
-          typeof event.featuredImage !== 'number' &&
-          event.featuredImage.url && (
-            <div className="relative aspect-[302/192] h-[192px] w-full overflow-hidden">
-              <Image
-                src={event.featuredImage.url}
-                alt={event.title || ''}
-                fill
-                className="rounded-md object-cover"
-              />
+    <div className="p-4 pt-0 md:px-24">
+      <div className="bg-cream/5 text-cream flex flex-col items-center gap-4 rounded-2xl p-4 md:flex-row-reverse md:pr-5 md:pb-7 md:pl-12 lg:pr-9 lg:pb-11 lg:pl-20 xl:mx-auto xl:max-w-[953px]">
+        <div>
+          {event.featuredImage &&
+            typeof event.featuredImage !== 'number' &&
+            event.featuredImage.url && (
+              <div className="relative aspect-[302/192] h-[192px] w-full overflow-hidden md:aspect-[195/164] md:h-[164px] lg:aspect-[325/274] lg:h-[274px]">
+                <Image
+                  src={event.featuredImage.url}
+                  alt={event.title || ''}
+                  fill
+                  className="rounded-md object-cover"
+                />
+              </div>
+            )}
+        </div>
+        <div className="flex flex-col gap-4 md:mb-1 md:gap-0">
+          <div className="md:flex md:flex-col-reverse">
+            <h2 className="font-heading truncate text-lg md:mb-2 md:text-xs lg:mb-4 lg:text-lg">
+              {event?.title}
+            </h2>
+            <div className="flex w-full justify-between md:mb-5 md:w-fit md:gap-[17.4px] lg:gap-7">
+              <NextAdventureGrade>3</NextAdventureGrade>
+              {event.startTime && event.endTime && (
+                <NextAdventureDate
+                  startTime={event.startTime}
+                  endTime={event.endTime}
+                />
+              )}
             </div>
-          )}
-        <h2 className="font-heading truncate text-lg">{event?.title}</h2>
-        <div className="flex w-full justify-between">
-          <NextAdventureGrade>3</NextAdventureGrade>
-          {event.startTime && event.endTime && (
-            <NextAdventureDate
-              startTime={event.startTime}
-              endTime={event.endTime}
-            />
-          )}
+          </div>
+          <div className="font-body text-sm italic md:mb-2 md:text-[9px] lg:mb-5 lg:text-sm">
+            {event?.description && <RichText data={event.description} />}
+          </div>
+          <NextAdventureLocation>{event?.location}</NextAdventureLocation>
+          <NextAdventureButtons eventId={event.id}></NextAdventureButtons>
         </div>
-        <div className="font-body text-sm italic">
-          {event?.description && <RichText data={event.description} />}
-        </div>
-        <NextAdventureLocation>{event?.location}</NextAdventureLocation>
-        <NextAdventureButtons eventId={event.id}></NextAdventureButtons>
       </div>
     </div>
   )
