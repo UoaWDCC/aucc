775409f6531a3d05ff089e672c0c6245d8e4b599
BlackPri0r <lawrenceli630@gmail.com>
2025-07-16 03:18:10 +1200

    REFACTOR: Added tablet/desktop responsiveness

--- src/app/(frontend)/_components/home/next-adventure/NextAdventureDate.tsx
+++ src/app/(frontend)/_components/home/next-adventure/NextAdventureDate.tsx
@@ -8,9 +8,9 @@ type DateProps = {
 export function NextAdventureDate({ startTime, endTime }: DateProps) {
   return (
     <div className="flex items-center gap-1">
-      <Calendar size={20} color="#EFEFE1" />
+      <Calendar className="h-5 w-5 text-[#EFEFE1] md:h-3 md:w-3 lg:h-5 lg:w-5" />
       <div>
-        <p>
+        <p className="font-unbounded text-sm md:text-[8px] lg:text-xs">
           {new Date(startTime).getDate()}-{new Date(endTime).getDate()}{' '}
           {new Date(startTime)
             .toLocaleDateString('en-US', { month: 'long' })
--- src/app/(frontend)/_components/home/next-adventure/NextAdventureLocation.tsx
+++ src/app/(frontend)/_components/home/next-adventure/NextAdventureLocation.tsx
@@ -6,13 +6,11 @@ type LocationProps = {
 
 export function NextAdventureLocation({ children }: LocationProps) {
   return (
-    <div className="flex w-full">
-      <MapPin
-        className="inline-block [transform:skewX(-10deg)]"
-        size={20}
-        color="#EFEFE1"
-      />
-      <p className="font-body w-full text-left italic">{children}</p>
+    <div className="lg:mb-[18px flex w-full md:mb-5">
+      <MapPin className="lg inline-block h-5 w-5 [transform:skewX(-10deg)] text-[#EFEFE1] md:h-[11px] md:w-[11px] lg:h-5 lg:w-5" />
+      <p className="font-body md:text-[9px w-full text-left text-sm italic lg:text-sm">
+        {children}
+      </p>
     </div>
   )
 }
