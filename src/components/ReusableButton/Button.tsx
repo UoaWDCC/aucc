10084c6666475225bfb3848832a2461b6779b15b
Warri0r2005 <dom.pietersen.1@gmail.com>
2025-07-17 17:56:06 +1200

    Create inital variants and update styles and link handling

--- src/components/ReusableButton/Button.tsx
+++ src/components/ReusableButton/Button.tsx
@@ -1,25 +1,53 @@
+import { kMaxLength } from 'buffer'
 import React from 'react'
 import Link from 'next/link'
 import clsx from 'clsx'
 
 type buttonProps = {
   text: string
-  href?: string
+  link?: string
   onClick?: () => void
   size: string
   className?: string
+  variant?: 'cream' | 'algae' | 'default'
 }
 
-const baseStyle = 'flex items-center justify-center text-center p-5 bg-white'
+const baseStyle = 'flex items-center justify-center text-center'
+
+const variantStyles: Record<NonNullable<buttonProps['variant']>, string> = {
+  cream:
+    'text-cream border-cream bg-transparent cursor-pointer rounded-4xl border-2 italic shadow-lg transition hover:bg-black/15',
+  algae: 'text-algae cursor-pointer rounded-3xl border-2 text-xs',
+  default: 'text-cream cursor-pointer bg-transparent border-2 rounded-4xl',
+}
 
 export default function Button({
   text,
-  href,
+  link,
   onClick,
   size,
   className,
+  variant,
 }: buttonProps) {
-  const classes = clsx(size, className, baseStyle)
+  const href = { link },
+    classes = clsx(
+      size,
+      className,
+      baseStyle,
+      variantStyles[variant ?? 'default'],
+    )
 
-  return <div className={classes}>{text}</div>
+  if (link) {
+    return (
+      <Link href={link} className={classes}>
+        {text}
+      </Link>
+    )
+  }
+
+  return (
+    <div className={classes} onClick={onClick}>
+      {text}
+    </div>
+  )
 }
