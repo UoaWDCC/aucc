97d88c826a453746d01e5fefbcb33e9f56f1bf77
BlackPri0r <lawrenceli630@gmail.com>
2025-07-23 05:57:19 +1200

    FIX: fixed event query and river grade

--- src/app/(frontend)/_components/home/next-adventure/NextAdventureLocation.tsx
+++ src/app/(frontend)/_components/home/next-adventure/NextAdventureLocation.tsx
@@ -6,9 +6,9 @@ type LocationProps = {
 
 export function NextAdventureLocation({ children }: LocationProps) {
   return (
-    <div className="lg:mb-[18px flex w-full md:mb-5">
-      <MapPin className="lg inline-block h-5 w-5 [transform:skewX(-10deg)] text-[#EFEFE1] md:h-[11px] md:w-[11px] lg:h-5 lg:w-5" />
-      <p className="font-body md:text-[9px w-full text-left text-sm italic lg:text-sm">
+    <div className="flex w-full md:mb-5 lg:mb-[18px]">
+      <MapPin className="lg inline-block h-5 w-5 text-[#EFEFE1] md:h-[11px] md:w-[11px] lg:h-5 lg:w-5" />
+      <p className="font-body w-full text-left text-sm italic md:text-[9px] lg:text-sm">
         {children}
       </p>
     </div>
