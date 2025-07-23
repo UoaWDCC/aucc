fbcfa76eede089ceb28c8a897db230f48cf7d7a1
BlackPri0r <lawrenceli630@gmail.com>
2025-07-23 12:24:34 +1200

    FEAT: Fallback component if no event is returned

--- src/app/(frontend)/_components/home/next-adventure/NextAdventureSection.tsx
+++ src/app/(frontend)/_components/home/next-adventure/NextAdventureSection.tsx
@@ -1,16 +1,17 @@
 import { getNextEvent } from '@/queries/events'
 import { NextAdventureCard } from './NextAdventureCard'
+import { NextAdventureFallback } from './NextAdventureFallback'
 import { NextAdventureHeader } from './NextAdventureHeader'
 import { NextAdventureSectionBackground } from './NextAdventureSectionBackground'
 
 export async function NextAdventureSection() {
   const event = await getNextEvent()
-  if (!event) return null
 
   return (
     <NextAdventureSectionBackground>
       <NextAdventureHeader />
-      <NextAdventureCard event={event} />
+      <NextAdventureFallback />
+      {event ? <NextAdventureCard event={event} /> : <NextAdventureFallback />}
     </NextAdventureSectionBackground>
   )
 }
