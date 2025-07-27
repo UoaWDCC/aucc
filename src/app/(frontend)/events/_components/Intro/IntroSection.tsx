61d991c44914897c0e023caff967442d251de6b8
f13322 <119908908+f13322@users.noreply.github.com>
2025-07-28 03:26:13 +1200

    fix text gap

--- src/app/(frontend)/events/_components/Intro/IntroSection.tsx
+++ src/app/(frontend)/events/_components/Intro/IntroSection.tsx
@@ -7,7 +7,7 @@ export function IntroSection() {
       <IntroImage alt="IntroImage" />
       <div className="text-cream relative z-1">
         <h1 className="font-heading flex text-center text-[140px]">Events</h1>
-        <h2 className="font-body -mt-5 w-80 pl-3 leading-5 tracking-tighter italic">
+        <h2 className="font-body -mt-10 w-80 pl-3 leading-5 tracking-tighter italic">
           From action-packed trips to small kayak training sessions - find our
           major events here.
         </h2>
