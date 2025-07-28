64e6facb8f8f157a4aa2ef0b8f0f314c8bb7e164
f13322 <119908908+f13322@users.noreply.github.com>
2025-07-29 00:41:47 +1200

    add sizing for mobile

--- src/app/(frontend)/events/_components/Intro/IntroSection.tsx
+++ src/app/(frontend)/events/_components/Intro/IntroSection.tsx
@@ -5,9 +5,11 @@ export function IntroSection() {
   return (
     <div className="relative flex h-[100vh] flex-col items-center justify-center align-middle">
       <IntroImage alt="IntroImage" />
-      <div className="text-cream relative z-1">
-        <h1 className="font-heading flex text-center text-[140px]">Events</h1>
-        <h2 className="font-body -mt-10 w-80 pl-3 leading-5 tracking-tighter italic">
+      <div className="text-cream relative z-1 flex flex-col items-center md:items-start">
+        <h1 className="font-heading flex justify-self-center text-center text-[60px] md:text-[140px]">
+          Events
+        </h1>
+        <h2 className="font-body -mt-4 w-60 pl-2 text-center text-xs leading-5 tracking-tighter italic md:-mt-10 md:w-80 md:pl-3 md:text-start md:text-base">
           From action-packed trips to small kayak training sessions - find our
           major events here.
         </h2>
