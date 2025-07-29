7de2026dec4630fa8c5a7f2eccb062ac8ed5d91c
Warri0r2005 <dom.pietersen.1@gmail.com>
2025-07-29 18:43:53 +1200

    Update styling to cva

--- src/components/ReusableButton/Button.tsx
+++ src/components/ReusableButton/Button.tsx
@@ -1,53 +1,41 @@
-import { kMaxLength } from 'buffer'
 import React from 'react'
 import Link from 'next/link'
-import clsx from 'clsx'
+import { cva } from 'class-variance-authority'
 
-type buttonProps = {
-  text: string
-  link?: string
-  onClick?: () => void
-  size: string
-  className?: string
-  variant?: 'cream' | 'algae' | 'default'
-}
+/*
+{
+  const baseStyle = 'flex items-center justify-center text-center'
 
-const baseStyle = 'flex items-center justify-center text-center'
-
-const variantStyles: Record<NonNullable<buttonProps['variant']>, string> = {
   cream:
     'text-cream border-cream bg-transparent cursor-pointer rounded-4xl border-2 italic shadow-lg transition hover:bg-black/15',
   algae: 'text-algae cursor-pointer rounded-3xl border-2 text-xs',
   default: 'text-cream cursor-pointer bg-transparent border-2 rounded-4xl',
 }
+*/
 
-export default function Button({
-  text,
-  link,
-  onClick,
-  size,
-  className,
-  variant,
-}: buttonProps) {
-  const href = { link },
-    classes = clsx(
-      size,
-      className,
-      baseStyle,
-      variantStyles[variant ?? 'default'],
-    )
+const buttonVariants = cva('flex items-center justify-center text-center', {
+  variants: {
+    intent: {
+      primary: ['text-algae', 'cursor-pointer', 'rounded-3xl', 'border-2'],
+      secondary: [
+        'text-cream',
+        'border-cream',
+        'bg-transparent',
+        'cursor-pointer',
+        'rounded-4xl',
+        'border-2',
+        'italic',
+        'shadow-lg',
+        'transition',
+        'hover:bg-black/15',
+      ],
+      tertiary: [],
+    },
+    size: {
+      sm: ['text-xs', 'py-1', 'px-2'],
+      md: ['text-base', 'py-2', '[px-4'],
+    },
+  },
+})
 
-  if (link) {
-    return (
-      <Link href={link} className={classes}>
-        {text}
-      </Link>
-    )
-  }
-
-  return (
-    <div className={classes} onClick={onClick}>
-      {text}
-    </div>
-  )
-}
+export default function Button() {}
