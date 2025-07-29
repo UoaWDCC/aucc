61733527bc0fe40b6119f22a77f3919d8ae494c7
Warri0r2005 <dom.pietersen.1@gmail.com>
2025-07-29 20:51:23 +1200

    Remove and add comments

--- src/components/ReusableButton/Button.tsx
+++ src/components/ReusableButton/Button.tsx
@@ -4,21 +4,10 @@ import { cva, VariantProps } from 'class-variance-authority'
 
 import { cn } from '@/lib/utils/cn'
 
-/*
-{
-  const baseStyle = 'flex items-center justify-center text-center'
-
-  cream:
-    'text-cream border-cream bg-transparent cursor-pointer rounded-4xl border-2 italic shadow-lg transition hover:bg-black/15',
-  algae: 'text-algae cursor-pointer rounded-3xl border-2 text-xs',
-  default: 'text-cream cursor-pointer bg-transparent border-2 rounded-4xl',
-}
-*/
-
 const buttonVariants = cva('flex items-center justify-center text-center', {
   variants: {
     intent: {
-      primary: ['text-algae', 'cursor-pointer', 'border-2', 'rounded-4xl'],
+      primary: ['text-algae', 'cursor-pointer', 'border-2', 'rounded-4xl'], // Default styled button
       secondary: [
         'text-cream',
         'border-cream',
@@ -51,6 +40,7 @@ const buttonVariants = cva('flex items-center justify-center text-center', {
       ],
     },
     size: {
+      // md is default size
       sm: ['text-xs', 'py-1', 'px-2'],
       md: ['text-base', 'py-2', 'px-4'],
       lg: ['text-2xl', 'py-3', 'px-6'],
