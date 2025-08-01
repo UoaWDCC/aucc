90a01b11a3a45faf52c0416b78bb1f8b9f69d323
avalee0215 <chaechae0215@gmail.com>
2025-08-01 17:19:38 +1200

    Update background. Fixing FlareIcon location

--- src/app/(frontend)/events/_components/Upcoming/UpcomingEventsText.tsx
+++ src/app/(frontend)/events/_components/Upcoming/UpcomingEventsText.tsx
@@ -1,10 +1,11 @@
-// UpcomingText.tsx
+import Link from 'next/link'
 
+/** icon on the title */
 function FlareIcon({ className }: { className?: string }) {
   return (
     <svg
-      width="27"
-      height="80"
+      width="30"
+      height="83"
       viewBox="0 0 27 80"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
@@ -31,20 +32,30 @@ function FlareIcon({ className }: { className?: string }) {
 
 export function UpcomingText() {
   return (
-    <div className="flex w-full flex-col items-center px-4 md:items-start md:pt-12 md:pr-[14%] xl:pt-31">
-      <div className="relative mt-11 mb-27.5 flex flex-row justify-center md:mb-2 xl:mr-6">
-        <h1 className="font-heading text-grass relative text-center text-3xl tracking-tighter md:text-3xl xl:text-5xl">
-          UPCOMING TRIPS
-          <FlareIcon className="absolute -top-4 -left-6 z-10" />
-        </h1>
-      </div>
+    <div className="flex w-full flex-col items-start px-12 pt-20 md:pt-20 md:pl-[10%] xl:pt-32 xl:pl-28">
+      <div className="flex flex-col gap-6">
+        {/* Title Container (Flare 기준 anchor) */}
+        <div className="relative w-fit">
+          <FlareIcon className="text-grass absolute -top-3 right-[-20px] z-10 scale-x-[-1] md:-top-4 md:-right-4" />
+          <h1 className="font-heading text-grass text-left text-3xl tracking-tighter md:text-4xl xl:text-5xl">
+            UPCOMING TRIPS
+          </h1>
+        </div>
 
-      <div className="font-body w-full max-w-130 text-center text-sm leading-5 italic md:max-w-93 md:text-end md:text-[10px] md:leading-4 xl:max-w-130 xl:text-sm xl:leading-5">
-        Here you&apos;ll find information on our major events for the year
-        ranging from action-packed trips to small kayak training
-        sessions. Updated information on upcoming trips will be circulated on
-        the email list and our social media pages. Check out previous events on
-        our Trip Reports page.
+        {/* Info */}
+        <p className="font-body max-w-sm text-sm leading-6 text-gray-800 italic">
+          Here you&apos;ll find information on our major events for the year
+          ranging from action-packed trips to small kayak training sessions.
+          Updated information on upcoming trips will be circulated on the email
+          list and our social media pages. Check out previous events on our{' '}
+          <Link
+            href="/trip-reports"
+            className="text-grass underline hover:text-[#475A30]"
+          >
+            Trip Reports
+          </Link>{' '}
+          page.
+        </p>
       </div>
     </div>
   )
