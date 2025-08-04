a6b8260fb14e53a4b109838b68ae656e17b05e48
avalee0215 <chaechae0215@gmail.com>
2025-08-04 17:30:52 +1200

    Finish tablet / Desktop

--- src/app/(frontend)/events/_components/Upcoming/UpcomingEvents.tsx
+++ src/app/(frontend)/events/_components/Upcoming/UpcomingEvents.tsx
@@ -5,10 +5,14 @@ import { UpcomingText } from './UpcomingEventsText'
 export function UpcomingSection() {
   return (
     <UpcomingBackground>
-      <div className="flex w-full flex-col items-start md:flex-row md:items-start xl:gap-20">
-        <div className="flex flex-col md:flex-row md:items-start md:gap-8">
-          <UpcomingText />
-          <UpcomingEventsBox />
+      <div className="flex w-full flex-col items-start px-4 md:px-8 xl:px-20">
+        <div className="flex w-full flex-col gap-y-10 md:flex-row md:items-start md:gap-x-12">
+          <div className="md:w-[60%] xl:w-[65%]">
+            <UpcomingText />
+          </div>
+          <div className="md:w-[40%] xl:w-[35%]">
+            <UpcomingEventsBox />
+          </div>
         </div>
       </div>
     </UpcomingBackground>
