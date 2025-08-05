28f6766f1724a7e72b179b3d1e1754c4fc2be171
Troyabbs <troy.abs28@gmail.com>
2025-08-06 11:42:06 +1200

    POC using main gallery

---
+++ src/app/(frontend)/events/[id]/_components/EventGallery/EventGalleryGrid.tsx
@@ -0,0 +1,39 @@
+import { getGallery } from '@/queries/gallery'
+
+export async function EventGalleryGrid() {
+  const { gallery } = await getGallery({ limit: 8 })
+
+  type GalleryItem = (typeof gallery)[0]
+
+  const repeatedGallery = [...Array(8)].map(
+    (_, i) => gallery[i % gallery.length],
+  )
+
+  const shuffledGallery = [...repeatedGallery].sort(() => Math.random() - 0.5)
+
+  const columns: GalleryItem[][] = [[], [], [], []]
+
+  shuffledGallery.forEach((item, idx) => {
+    columns[idx % 4].push(item)
+  })
+
+  return (
+    <div className="grid grid-cols-4 gap-2 p-4">
+      {columns.map((col, idx) => (
+        <div
+          key={idx}
+          className={`flex flex-col gap-2 ${idx % 2 === 0 ? 'justify-start' : 'justify-end'}`}
+        >
+          {col.map((item, i) => (
+            <img
+              key={`${item.id}-${idx}-${i}`}
+              src={item.image.url ?? ''}
+              alt=""
+              className="h-auto w-full object-cover"
+            />
+          ))}
+        </div>
+      ))}
+    </div>
+  )
+}
