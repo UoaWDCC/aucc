cf1e9df16e9a90c2eb1264445e6a9662939b24b7
psae970 <psae970@aucklanduni.ac.nz>
2025-08-06 14:41:31 +1200

    changes cursor to pointer for SeeTripGuide button

--- src/app/(frontend)/events/[id]/_components/SpecificEventTicketInfo/SeeTripGuide.tsx
+++ src/app/(frontend)/events/[id]/_components/SpecificEventTicketInfo/SeeTripGuide.tsx
@@ -4,7 +4,7 @@ import { ArrowRight } from 'lucide-react'
 export function SeeTripGuide() {
   return (
     <Link href="/resources">
-      <button className="text-cream border-cream font-unbounded text-md flex items-center text-left text-sm">
+      <button className="text-cream border-cream font-unbounded text-md flex cursor-pointer items-center text-left text-sm">
         SEE TRIP GUIDE
         <ArrowRight />
       </button>
