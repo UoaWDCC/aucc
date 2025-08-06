1124441d267bc84722908994b789bfbbfcc032ec
Warri0r2005 <dom.pietersen.1@gmail.com>
2025-08-06 14:51:14 +1200

    Add Header and spacings with base styles

--- src/app/(frontend)/events/_components/PastAdventures/PastAdventuresSection.tsx
+++ src/app/(frontend)/events/_components/PastAdventures/PastAdventuresSection.tsx
@@ -1,7 +1,11 @@
 export function PastAdventuresSection() {
   return (
-    <div>
-      <h2>Past Adventures</h2>
+    <div className="bg-abyss height-auto width-full flex flex-col items-center justify-center">
+      <div className="width-full">
+        <h1 className="text-cream font-heading mt-31 text-5xl tracking-tighter">
+          Past Adventures
+        </h1>
+      </div>
     </div>
   )
 }
