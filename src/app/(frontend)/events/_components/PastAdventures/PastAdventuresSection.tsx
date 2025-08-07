489107bf9f855140d6fd0e51f1a95616ea6a4efe
Warri0r2005 <dom.pietersen.1@gmail.com>
2025-08-07 20:09:36 +1200

    Add line growth functionality

--- src/app/(frontend)/events/_components/PastAdventures/PastAdventuresSection.tsx
+++ src/app/(frontend)/events/_components/PastAdventures/PastAdventuresSection.tsx
@@ -1,16 +1,18 @@
 import Image from 'next/image'
 
-import image from '@/assets/canoe.webp'
+import image from '@/assets/kayak.webp'
 
 export function PastAdventuresSection() {
   return (
     <div className="bg-abyss mt-31 h-auto w-full">
-      <div className="flex w-full items-center justify-center gap-6">
-        <div className="bg-cream h-[1.3px] flex-grow"></div>
-        <h1 className="text-cream font-heading text-5xl tracking-tighter">
+      <div className="flex w-full items-center">
+        <div className="bg-cream h-[1.3px] flex-grow lg:pl-[339px]"></div>
+
+        <h1 className="text-cream font-heading px-6 text-5xl tracking-tighter">
           Past Adventures
         </h1>
-        {/* <Image src={image} alt="Canoe Image" /> */}
+        <div className="bg-cream h-[1.3px] flex-grow"></div>
+        <Image src={image} alt="Kayak Image" className="h-auto w-auto" />
       </div>
     </div>
   )
