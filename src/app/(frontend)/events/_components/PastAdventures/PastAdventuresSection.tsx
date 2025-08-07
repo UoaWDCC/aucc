e0f06ea7542c066410832ebca7adf247ea7a7bf2
Warri0r2005 <dom.pietersen.1@gmail.com>
2025-08-07 19:47:11 +1200

    Allow for flex growth and justify items correctly

--- src/app/(frontend)/events/_components/PastAdventures/PastAdventuresSection.tsx
+++ src/app/(frontend)/events/_components/PastAdventures/PastAdventuresSection.tsx
@@ -1,10 +1,16 @@
+import Image from 'next/image'
+
+import image from '@/assets/canoe.webp'
+
 export function PastAdventuresSection() {
   return (
-    <div className="bg-abyss height-auto width-full flex flex-col items-center justify-center">
-      <div className="width-full">
-        <h1 className="text-cream font-heading mt-31 text-5xl tracking-tighter">
+    <div className="bg-abyss mt-31 h-auto w-full">
+      <div className="flex w-full items-center justify-center gap-6">
+        <div className="bg-cream h-[1.3px] flex-grow"></div>
+        <h1 className="text-cream font-heading text-5xl tracking-tighter">
           Past Adventures
         </h1>
+        {/* <Image src={image} alt="Canoe Image" /> */}
       </div>
     </div>
   )
