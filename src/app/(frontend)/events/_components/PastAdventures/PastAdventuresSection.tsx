4891437f6efbc1205ed24158f6d319576ff0db9a
Warri0r2005 <dom.pietersen.1@gmail.com>
2025-08-07 20:19:12 +1200

    update styling

--- src/app/(frontend)/events/_components/PastAdventures/PastAdventuresSection.tsx
+++ src/app/(frontend)/events/_components/PastAdventures/PastAdventuresSection.tsx
@@ -6,13 +6,17 @@ export function PastAdventuresSection() {
   return (
     <div className="bg-abyss mt-31 h-auto w-full">
       <div className="flex w-full items-center">
-        <div className="bg-cream h-[1.3px] flex-grow lg:pl-[339px]"></div>
+        <div className="bg-cream hidden h-[1.3px] flex-grow md:block xl:pl-[339px]"></div>
 
-        <h1 className="text-cream font-heading px-6 text-5xl tracking-tighter">
+        <h1 className="text-cream font-heading px-6 text-5xl tracking-tighter whitespace-nowrap">
           Past Adventures
         </h1>
-        <div className="bg-cream h-[1.3px] flex-grow"></div>
-        <Image src={image} alt="Kayak Image" className="h-auto w-auto" />
+        <div className="bg-cream hidden h-[1.3px] flex-grow xl:block"></div>
+        <Image
+          src={image}
+          alt="Kayak Image"
+          className="hidden h-auto w-auto md:block"
+        />
       </div>
     </div>
   )
