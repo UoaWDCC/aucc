f21b866cd91de77a19c6ff6e8d43adc56bac3c48
Mai Pham <phamngocmei0912@gmail.com>
2025-08-12 17:19:01 +1200

    add parallax effect to images of intro and looking for us section

--- src/app/(frontend)/_components/home/intro/IntroImages.tsx
+++ src/app/(frontend)/_components/home/intro/IntroImages.tsx
@@ -1,4 +1,8 @@
+'use client'
+
+import { useEffect, useState } from 'react'
 import Image from 'next/image'
+import { Parallax, ParallaxProvider } from 'react-scroll-parallax'
 
 import paddle from '@/assets/paddle.webp'
 import welcome01 from '@/assets/welcome-01.webp'
@@ -6,42 +10,55 @@ import welcome02 from '@/assets/welcome-02.webp'
 import welcome03 from '@/assets/welcome-03.webp'
 
 export function IntroImages() {
+  const [isSmall, setIsSmall] = useState(false)
+
+  useEffect(() => {
+    const checkScreen = () => setIsSmall(window.innerWidth < 768)
+    checkScreen()
+    window.addEventListener('resize', checkScreen)
+    return () => window.removeEventListener('resize', checkScreen)
+  }, [])
   return (
     <>
-      <div className="relative mt-31 mr-auto ml-auto h-auto max-w-140 md:-mt-44 md:ml-0 xl:-mt-71">
-        <div className="absolute -left-4 aspect-[54/44] w-[58%] overflow-hidden rounded-2xl md:-left-11 md:h-44 md:w-55 xl:h-74 xl:w-92">
+      <ParallaxProvider>
+        <Parallax translateY={isSmall ? ['0px', '-100px'] : ['25px', '-130px']}>
+          <div className="relative mt-31 mr-auto ml-auto h-auto max-w-140 md:-mt-44 md:ml-0 xl:-mt-71">
+            <div className="absolute -left-4 aspect-[54/44] w-[58%] overflow-hidden rounded-2xl md:-left-11 md:h-44 md:w-55 xl:h-74 xl:w-92">
+              <Image
+                src={welcome01}
+                fill
+                className="object-cover"
+                alt="Intro picture 1"
+              />
+            </div>
+            <div className="absolute right-4 z-1 mt-[21%] aspect-[28/43] w-[30%] overflow-hidden rounded-2xl md:left-26 md:mt-47 md:h-37 md:w-24 xl:left-51 xl:mt-79 xl:h-62 xl:w-40">
+              <Image
+                src={welcome03}
+                fill
+                className="object-cover"
+                alt="Intro picture 3"
+              />
+            </div>
+          </div>
+
+          <div className="absolute right-[32%] z-2 mt-[50%] aspect-[20/25] w-[22%] overflow-hidden rounded-2xl md:left-46 md:mt-38 md:h-22 md:w-18 xl:left-85 xl:mt-64 xl:h-37 xl:w-30">
+            <Image
+              src={welcome02}
+              fill
+              className="object-cover"
+              alt="Intro picture 2"
+            />
+          </div>
+        </Parallax>
+        <div className="xl: absolute top-25 -left-9 h-[calc(0.21*65vw)] w-[65%] min-w-75 origin-top-right rotate-7 overflow-hidden md:-top-2 md:right-10 md:left-auto md:mr-[7%] md:h-20 md:w-115 md:origin-top-right md:-rotate-90 xl:mt-4 xl:w-182">
           <Image
-            src={welcome01}
+            src={paddle}
             fill
-            className="object-cover"
-            alt="Intro picture 1"
+            className="-scale-y-100 object-cover"
+            alt="Paddle"
           />
         </div>
-        <div className="absolute right-[32%] z-2 mt-[50%] aspect-[20/25] w-[22%] overflow-hidden rounded-2xl md:left-46 md:mt-38 md:h-22 md:w-18 xl:left-85 xl:mt-64 xl:h-37 xl:w-30">
-          <Image
-            src={welcome02}
-            fill
-            className="object-cover"
-            alt="Intro picture 2"
-          />
-        </div>
-        <div className="absolute right-4 z-1 mt-[21%] aspect-[28/43] w-[30%] overflow-hidden rounded-2xl md:left-26 md:mt-47 md:h-37 md:w-24 xl:left-51 xl:mt-79 xl:h-62 xl:w-40">
-          <Image
-            src={welcome03}
-            fill
-            className="object-cover"
-            alt="Intro picture 3"
-          />
-        </div>
-      </div>
-      <div className="xl: absolute top-25 -left-9 h-[calc(0.21*65vw)] w-[65%] min-w-75 origin-top-right rotate-7 overflow-hidden md:-top-2 md:right-10 md:left-auto md:mr-[7%] md:h-20 md:w-115 md:origin-top-right md:-rotate-90 xl:mt-4 xl:w-182">
-        <Image
-          src={paddle}
-          fill
-          className="-scale-y-100 object-cover"
-          alt="Paddle"
-        />
-      </div>
+      </ParallaxProvider>
     </>
   )
 }
