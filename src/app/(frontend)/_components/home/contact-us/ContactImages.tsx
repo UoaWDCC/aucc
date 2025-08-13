aeedfdb21f796875cb132a625133ee2f38a8cf12
dyzhuu <davidyzhu1973@gmail.com>
2025-08-13 18:35:35 +1200

    individual image parallax

--- src/app/(frontend)/_components/home/contact-us/ContactImages.tsx
+++ src/app/(frontend)/_components/home/contact-us/ContactImages.tsx
@@ -9,24 +9,24 @@ import lookingForUs02 from '@/assets/looking-for-us-02.webp'
 import lookingForUs03 from '@/assets/looking-for-us-03.webp'
 
 export function ContactImages() {
-  const [isMedium, setIsMedium] = useState(false)
+  const [isDesktop, setIsDesktop] = useState(false)
 
   useEffect(() => {
-    const checkScreen = () => setIsMedium(window.innerWidth < 1024)
+    const checkScreen = () => setIsDesktop(window.innerWidth < 1024)
     checkScreen()
     window.addEventListener('resize', checkScreen)
     return () => window.removeEventListener('resize', checkScreen)
   }, [])
+
   return (
     <div className="relative mt-18 mb-36 w-full md:mt-16 md:mb-28 lg:mt-[-20px] lg:mr-[2%] lg:mb-16 lg:ml-auto lg:w-[420px] xl:mt-[-20px] xl:mr-[2%] xl:w-[500px]">
       <div className="relative mt-20 h-auto w-full md:mt-12 lg:mt-0">
         <ParallaxProvider>
-          <Parallax
-            translateY={isMedium ? ['0px', '-115px'] : ['0px', '-200px']}
-          >
-            <div className="relative mb-16 h-[min(500px,60vw)] w-full md:mb-12 lg:mb-10 xl:mb-10">
-              <div className="relative mr-[1%] ml-auto w-fit">
-                {/* image 2 */}
+          <div className="relative mb-16 h-[min(500px,60vw)] w-full md:mb-12 lg:mb-10 xl:mb-10">
+            <div className="relative mr-[1%] ml-auto w-fit">
+              <Parallax
+                translateY={isDesktop ? ['0px', '-180px'] : ['0px', '-350px']}
+              >
                 <div className="relative aspect-[11/16] w-[38vw] max-w-[230px] overflow-hidden rounded-2xl xl:w-[30vw] xl:max-w-[260px]">
                   <Image
                     src={lookingForUs02}
@@ -35,8 +35,11 @@ export function ContactImages() {
                     className="object-cover"
                   />
                 </div>
+              </Parallax>
 
-                {/* image 3 */}
+              <Parallax
+                translateY={isDesktop ? ['0px', '-300px'] : ['0px', '-500px']}
+              >
                 <div className="absolute top-[55%] right-[80%] z-10 aspect-[2/3] w-[24vw] max-w-[140px] overflow-hidden rounded-2xl xl:w-[20vw] xl:max-w-[160px]">
                   <Image
                     src={lookingForUs03}
@@ -45,9 +48,13 @@ export function ContactImages() {
                     className="object-cover"
                   />
                 </div>
-              </div>
+              </Parallax>
             </div>
-            {/* image 1 */}
+          </div>
+
+          <Parallax
+            translateY={isDesktop ? ['0px', '-120px'] : ['0px', '-250px']}
+          >
             <div className="relative mt-[6%] flex justify-end pr-[3%] md:mt-[4%] lg:mt-[1%]">
               <div className="w-[30%] max-w-[160px] rounded-2xl xl:w-[36%] xl:max-w-[180px]">
                 <Image
