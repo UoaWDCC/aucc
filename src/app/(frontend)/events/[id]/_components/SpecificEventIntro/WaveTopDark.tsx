2db8bda4e3f27d0eb020d2c5f488f55ade5f4e4d
psae970 <psae970@aucklanduni.ac.nz>
2025-08-13 13:53:37 +1200

    95% complete

---
+++ src/app/(frontend)/events/[id]/_components/SpecificEventIntro/WaveTopDark.tsx
@@ -0,0 +1,28 @@
+import { cn } from '@/lib/utils/cn'
+
+interface WaveTopDarkProps {
+  className?: string
+}
+
+export function WaveTopDark({ className }: WaveTopDarkProps) {
+  return (
+    <div className="absolute top-0 -z-40 overflow-hidden">
+      <svg
+        width="1280"
+        height="495"
+        viewBox="0 0 1280 495"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        className={cn('w-full', className)}
+        preserveAspectRatio="none"
+      >
+        <rect
+          width="1306.65"
+          height="870.212"
+          transform="matrix(-1 0 0 1 1293.65 -376)"
+          fill="#1E2A29"
+        />
+      </svg>
+    </div>
+  )
+}
