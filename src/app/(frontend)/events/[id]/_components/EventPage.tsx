5f758cd2d9b3e202dc3d416d840a570a46c8a9be
phawatboom <psae970@aucklanduni.ac.nz>
2025-08-17 20:34:51 +1200

    Specific event page intro section (block 1) (#144)
    * 95% complete
    
    merged tiketinfosection with SpecificEventinfoSection
    
    * feat: finished specific_intro_section_block1
    
    * fix: pin the time zone in formatDateRange function
    
    * fix: attempt to fix page not showing anything

--- src/app/(frontend)/events/[id]/_components/EventPage.tsx
+++ src/app/(frontend)/events/[id]/_components/EventPage.tsx
@@ -2,11 +2,13 @@ import Image from 'next/image'
 import { RichText } from '@payloadcms/richtext-lexical/react'
 
 import type { Event, Tag } from '@/payload-types'
+import { EventDTO } from '@/queries/events'
 import { EventGallerySection } from './EventGallery/EventGallerySection'
+import { SpecificEventIntroSection } from './SpecificEventIntro/SpecificEventIntroSection'
 import { TicketInformationSection } from './SpecificEventTicketInfo/TicketInfo'
 
 interface EventPageProps {
-  event: Event
+  event: EventDTO
 }
 
 export function EventPage({ event }: EventPageProps) {
@@ -15,41 +17,10 @@ export function EventPage({ event }: EventPageProps) {
       ? (event.tag as Tag).name
       : undefined
   return (
-    <div>
-      <section className="p-4">
-        <h1 className="text-center text-2xl font-bold text-white">
-          {event.title}
-        </h1>
-        <div className="mt-4 block border p-2">
-          <div className="h-144 p-2">
-            {event.featuredImage &&
-              typeof event.featuredImage !== 'number' &&
-              event.featuredImage.url && (
-                <Image
-                  src={event.featuredImage.url}
-                  alt={event.title || ''}
-                  className="h-full w-full object-cover"
-                  width={300}
-                  height={200}
-                />
-              )}
-          </div>
-          <div className="p-2 text-white">
-            {event.description && <RichText data={event.description} />}
-            <div className="mt-4 text-center">
-              {event.startTime && (
-                <p>Start: {new Date(event.startTime).toLocaleString()}</p>
-              )}
-              {event.endTime && (
-                <p>End: {new Date(event.endTime).toLocaleString()}</p>
-              )}
-              {event.location && <p>Location: {event.location}</p>}
-            </div>
-          </div>
-        </div>
-      </section>
+    <>
+      <SpecificEventIntroSection event={event} />
       <TicketInformationSection event={event} />
       {tagName && <EventGallerySection tagName={tagName} />}
-    </div>
+    </>
   )
 }
