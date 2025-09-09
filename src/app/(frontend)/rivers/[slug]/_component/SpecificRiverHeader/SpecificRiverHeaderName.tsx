b04b6afa94af8b0d9d1a7e0eb9f7a52c27c08971
f13322 <119908908+f13322@users.noreply.github.com>
2025-09-09 23:30:15 +1200

    fix heading alignment

--- src/app/(frontend)/rivers/[slug]/_component/SpecificRiverHeader/SpecificRiverHeaderName.tsx
+++ src/app/(frontend)/rivers/[slug]/_component/SpecificRiverHeader/SpecificRiverHeaderName.tsx
@@ -6,7 +6,7 @@ export function SpecificRiverHeaderName({
   name,
 }: SpecificRiverHeaderNameProps) {
   return (
-    <div className="font-heading text-cream mr-5 ml-auto flex h-60 w-[73%] max-w-100 items-center justify-items-end text-right text-2xl tracking-tighter md:mr-34 md:h-115 md:w-[60%] md:max-w-177 md:text-5xl">
+    <div className="font-heading text-cream mr-5 ml-auto flex h-60 w-[73%] max-w-100 items-center justify-end text-right text-2xl tracking-tighter md:mr-34 md:h-115 md:w-[60%] md:max-w-177 md:text-5xl">
       {name}
     </div>
   )
