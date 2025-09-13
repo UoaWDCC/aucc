8be9fbf1ceac584800b39775174584d706d378eb
phawatboom <psae970@aucklanduni.ac.nz>
2025-09-13 22:26:34 +1200

    chore: improve compile time (optimize Tailwind + TS.config) (#159)
    * tells Tailwind where to look for class names to generate only what we use
    
    * chore(build): optimize compile time with Tailwind + TS config updates
    
    * fix(ui): resolve overlapping box on small screens

--- src/app/(frontend)/tailwind.config.ts
+++ src/app/(frontend)/tailwind.config.ts
@@ -1,6 +1,15 @@
 import type { Config } from 'tailwindcss'
 
 export default {
+  content: [
+    './src/app/(frontend)/**/*.{ts,tsx,mdx}',
+    './src/components/**/*.{ts,tsx,mdx}',
+    './src/lib/utils/**/*.{ts,tsx}',
+    './src/assets/**/*.{ts,tsx,mdx}', // add (Logo, etc.)
+    './src/globals/**/*.{ts,tsx,mdx}', // add if any Tailwind classes here
+    './stories/**/*.{ts,tsx,mdx}', // uncomment if you want Storybook scanning
+    './src/app/(payload)/**/*.{ts,tsx}', // uncomment if admin needs Tailwind
+  ],
   theme: {
     extend: {
       typography: {
