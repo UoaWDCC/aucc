3c978ae37e1b2c26bc51f3b41e369b6bb48e0b24
Mai Pham <phamngocmei0912@gmail.com>
2025-09-30 23:16:06 +1300

    minor change

--- src/app/(frontend)/rivers/page.tsx
+++ src/app/(frontend)/rivers/page.tsx
@@ -5,5 +5,11 @@ import { RiversPage } from './_components/RiversPage'
 export default async function Page() {
   const { rivers } = await getRivers()
   const { headerImage, introText } = await getRiversGlobal()
-  return <RiversPage headerImage={headerImage} introText={introText} />
+  return (
+    <RiversPage
+      headerImage={headerImage}
+      introText={introText}
+      rivers={rivers}
+    />
+  )
 }
