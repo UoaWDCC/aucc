d806603b10d14cdfe17e545e675725b73eba0bdd
Warri0r2005 <dom.pietersen.1@gmail.com>
2025-10-19 12:54:48 +1300

    contact us text

--- src/app/(frontend)/about/_components/contact-us/ContactUsSection.tsx
+++ src/app/(frontend)/about/_components/contact-us/ContactUsSection.tsx
@@ -4,6 +4,7 @@ export function ContactUsSection() {
       <div className="text-cream font-heading relative z-30 -translate-y-28 text-center text-5xl md:text-6xl">
         Contact Us
       </div>
+      <div className="bg-abyss absolute right-0 bottom-0 z-50 h-80 w-80"></div>
     </div>
   )
 }
