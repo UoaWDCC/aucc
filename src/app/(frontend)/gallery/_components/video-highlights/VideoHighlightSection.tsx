4d2894eb8e7b4c09ccf4b05a13e0b13606815536
Yush <86692796+DeletedUser9@users.noreply.github.com>
2026-05-05 14:08:49 +1200

    feat: added video highlights arrow & made curve smoother

--- src/app/(frontend)/gallery/_components/video-highlights/VideoHighlightSection.tsx
+++ src/app/(frontend)/gallery/_components/video-highlights/VideoHighlightSection.tsx
@@ -1,12 +1,15 @@
 import { VideoHighlightCard } from './VideoHighlightCard'
+import { VideoHighightsArrow } from './VideoHighlightsArrow'
 import { VideoHighlightsCurve } from './VideoHighlightsCurve'
 
 export function VideoHighlightSection() {
   return (
-    <section className="relative bg-[#89ACAD] px-6 pt-12 pb-20">
+    <section className="relative w-full bg-[#89ACAD] px-6 pt-12 pb-20">
+      <VideoHighlightsCurve />
       <div className="mx-auto max-w-5xl">
-        <div className="mb-8 flex justify-center md:justify-end">
-          <h2 className="text-center text-3xl font-extrabold tracking-widest text-white md:text-4xl">
+        <div className="mb-8 flex justify-center md:justify-end md:gap-6">
+          <VideoHighightsArrow />
+          <h2 className="text-right text-3xl font-extrabold tracking-widest text-[#EFEFE1] md:text-4xl">
             VIDEO
             <br />
             HIGHLIGHTS
