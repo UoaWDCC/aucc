ed58dc3e9faf110a69b63cfc7aeca82051316e69
Carlbcm1 <cmis055@aucklanduni.ac.nz>
2026-05-11 14:16:29 +1200

    feat: correct responsiveness and gradient

--- src/app/(frontend)/merchandise/_components/MerchandisePage.tsx
+++ src/app/(frontend)/merchandise/_components/MerchandisePage.tsx
@@ -10,18 +10,18 @@ export function MerchandisePage() {
           fill
           priority
           sizes="100vw"
-          className="object-cover object-top brightness-90"
+          className="object-cover object-top"
         />
 
-        <div className="absolute inset-0 bg-gradient-to-t from-[#26342c] from-10% to-[#90A1A5]/15 opacity-75" />
+        <div className="absolute inset-0 bg-gradient-to-t from-[#26342c] from-10% to-[#90A1A5]/0 opacity-80 brightness-90" />
 
-        <div className="absolute inset-0 z-10 mt-8 flex items-center justify-center px-4 md:mt-0">
+        <div className="absolute top-[60%] left-1/2 z-10 -translate-x-1/2 -translate-y-1/2 px-4 sm:top-[50%] md:top-[50%] lg:top-[45%]">
           <div className="flex flex-col items-start">
-            <h1 className="font-heading text-4xl leading-none text-[#EFEDE3] sm:text-7xl md:text-8xl lg:text-9xl xl:text-[140px]">
+            <h1 className="font-heading text-3xl leading-none text-[#EFEDE3] min-[450px]:text-5xl sm:text-7xl md:text-8xl lg:text-[140px]">
               MERCH
             </h1>
 
-            <p className="mt-1 translate-x-2 text-[10px] text-[#EFEDE3] italic md:text-base">
+            <p className="text-[8px] text-[#EFEDE3] italic sm:translate-x-1 sm:text-xs md:translate-x-2 md:text-base">
               Past AUCC merchandise!
             </p>
           </div>
