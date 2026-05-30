ab21de9232e938a302286e0c7410db4131011f75
Olivia Kusuma <okus245@aucklanduni.ac.nz>
2026-05-31 11:01:58 +1200

    Update GalleryHeaderImage

--- src/app/(frontend)/gallery/_components/header/GalleryHeaderImage.tsx
+++ src/app/(frontend)/gallery/_components/header/GalleryHeaderImage.tsx
@@ -9,7 +9,7 @@ export function GalleryHeaderImage({ media }: GalleryHeaderImageProps) {
   return (
     <div className="absolute inset-0 size-full overflow-hidden select-none">
       <PayloadImage media={media} className="object-cover" />
-      <div className="absolute inset-0 z-1 size-full bg-gradient-to-t from-[#26342c] from-10% to-[#90A1A5]/0 opacity-80 brightness-90" />
+      <div className="absolute inset-0 z-1 size-full bg-gradient-to-t from-[#1E2A29] from-10% to-[#90A1A5]/0 opacity-80 brightness-90" />
     </div>
   )
 }
