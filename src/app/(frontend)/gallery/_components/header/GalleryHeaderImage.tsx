059aa3957a1ad36ed9171dc701d344801430f670
Olivia Kusuma <okus245@aucklanduni.ac.nz>
2026-06-02 00:46:14 +1200

    Fetch Gallery Header Image from Global (#221)
    * modify gallery header to fetch image from cms
    
    * Update GalleryHeaderImage
    
    * pull from main & changes made based on pull request comments.

--- src/app/(frontend)/gallery/_components/header/GalleryHeaderImage.tsx
+++ src/app/(frontend)/gallery/_components/header/GalleryHeaderImage.tsx
@@ -1,17 +1,15 @@
-import Image from 'next/image'
+import { PayloadImage } from '@/components/PayloadImage'
+import { Media } from '@/payload-types'
 
-import galleryHeading from '@/assets/gallery-heading.png'
+type GalleryHeaderImageProps = {
+  media: Media | null | undefined
+}
 
-export function GalleryHeaderImage() {
+export function GalleryHeaderImage({ media }: GalleryHeaderImageProps) {
   return (
     <div className="absolute inset-0 size-full overflow-hidden select-none">
-      <Image
-        src={galleryHeading}
-        alt="Gallery header"
-        fill
-        className="object-cover object-top"
-      />
-      <div className="absolute inset-0 z-1 size-full bg-gradient-to-t from-[#26342c] from-10% to-[#90A1A5]/0 opacity-80 brightness-90" />
+      <PayloadImage media={media ?? undefined} className="object-cover" />
+      <div className="absolute inset-0 z-1 size-full bg-gradient-to-t from-[#1E2A29] from-10% to-[#90A1A5]/0 opacity-80 brightness-90" />
     </div>
   )
 }
