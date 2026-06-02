069f867791644788a24733149e77f69e7b1af3ea
Jonathan Wu <158560545+yrska@users.noreply.github.com>
2026-06-02 15:11:38 +1200

    Revert "Fetch Gallery Header Image from Global (#221)"
    This reverts commit 059aa3957a1ad36ed9171dc701d344801430f670.

--- src/app/(frontend)/gallery/_components/GalleryPage.tsx
+++ src/app/(frontend)/gallery/_components/GalleryPage.tsx
@@ -1,17 +1,12 @@
-import { Media } from '@/payload-types'
 import { GallerySubheading } from './gallery-images/GallerySubheading'
 import { GallerySpotlightSection } from './gallery-spotlight/GallerySpotlightSection'
 import { GalleryHeaderSection } from './header/GalleryHeaderSection'
 import { VideoHighlightSection } from './video-highlights/VideoHighlightSection'
 
-type GalleryPageProps = {
-  headerImage: Media | null
-}
-
-export function GalleryPage({ headerImage }: GalleryPageProps) {
+export function GalleryPage() {
   return (
     <>
-      <GalleryHeaderSection headerImage={headerImage} />
+      <GalleryHeaderSection />
       <GallerySpotlightSection />
       <VideoHighlightSection />
       <GallerySubheading />
--- src/app/(frontend)/gallery/_components/header/GalleryHeaderImage.tsx
+++ src/app/(frontend)/gallery/_components/header/GalleryHeaderImage.tsx
@@ -1,15 +1,17 @@
-import { PayloadImage } from '@/components/PayloadImage'
-import { Media } from '@/payload-types'
+import Image from 'next/image'
 
-type GalleryHeaderImageProps = {
-  media: Media | null | undefined
-}
+import galleryHeading from '@/assets/gallery-heading.png'
 
-export function GalleryHeaderImage({ media }: GalleryHeaderImageProps) {
+export function GalleryHeaderImage() {
   return (
     <div className="absolute inset-0 size-full overflow-hidden select-none">
-      <PayloadImage media={media ?? undefined} className="object-cover" />
-      <div className="absolute inset-0 z-1 size-full bg-gradient-to-t from-[#1E2A29] from-10% to-[#90A1A5]/0 opacity-80 brightness-90" />
+      <Image
+        src={galleryHeading}
+        alt="Gallery header"
+        fill
+        className="object-cover object-top"
+      />
+      <div className="absolute inset-0 z-1 size-full bg-gradient-to-t from-[#26342c] from-10% to-[#90A1A5]/0 opacity-80 brightness-90" />
     </div>
   )
 }
