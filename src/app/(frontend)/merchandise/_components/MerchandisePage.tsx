f204533e7c3946509fe78f7bd9a494b704adef23
Carl misquitta <131878928+carlmisquitta@users.noreply.github.com>
2026-06-04 16:16:48 +1200

    Carl/199-built-showcase-section (#238)
    * FEAT: completed showcase responsivness for all breakpoints
    
    * fixing merge conflicts
    
    * FIX: fixing merge conflict
    
    * fix: edit gallery image tag from img to nextjs Image
    
    ---------
    
    Co-authored-by: Carlbcm1 <cmis055@aucklanduni.ac.nz>
    Co-authored-by: yrska <jonathanw15382@gmail.com>

--- src/app/(frontend)/merchandise/_components/MerchandisePage.tsx
+++ src/app/(frontend)/merchandise/_components/MerchandisePage.tsx
@@ -1,10 +1,12 @@
 import { MerchandiseFooterSection } from './footer/MerchandiseFooterSection'
 import { MerchandiseHeaderSection } from './header/MerchandiseHeaderSection'
+import MerchandiseShowcaseSection from './header/MerchandiseShowcaseSection'
 
 export function MerchandisePage() {
   return (
     <>
       <MerchandiseHeaderSection />
+      <MerchandiseShowcaseSection />
       <MerchandiseFooterSection />
     </>
   )
