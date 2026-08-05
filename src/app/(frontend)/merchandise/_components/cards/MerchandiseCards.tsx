e2d474f72f51546e5688543c41288598a0f526cf
Dylan Bhadresa <db20011@my.westlake.school.nz>
2026-08-05 23:03:26 +1200

    Fixed merch page

---
+++ src/app/(frontend)/merchandise/_components/cards/MerchandiseCards.tsx
@@ -0,0 +1,30 @@
+import imagePlaceholder from '@/assets/tshirt-image.png'
+import { MerchItemCard } from './MerchItemCard'
+
+export default function MerchandiseCards() {
+  return (
+    <div>
+      <div className="mx-auto max-w-5xl space-y-12 px-6 py-12">
+        <MerchItemCard
+          variant="image-left"
+          className="bg-white"
+          heading="T-Shirts"
+          imageSrc={imagePlaceholder}
+        />
+        <MerchItemCard
+          variant="image-right"
+          className="bg-white"
+          heading="Stickers"
+          imageSrc={imagePlaceholder}
+          body="If you're interested contact our media officers at social@aucc.org.nz for details."
+        />
+        <MerchItemCard
+          variant="image-with-caption"
+          className="bg-[#7F9F66]"
+          heading="More Towel Ponchos!"
+          imageSrc={imagePlaceholder}
+        />
+      </div>
+    </div>
+  )
+}
