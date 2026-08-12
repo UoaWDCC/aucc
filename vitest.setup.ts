2f52db7420f68ba44e4630be822273a51da54311
Olivia Kusuma <okus245@aucklanduni.ac.nz>
2026-08-12 19:12:24 +1200

    Swims list component build & create test cases for components.

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,6 +1,9 @@
+import '@testing-library/jest-dom/vitest'
+
 class MockIntersectionObserver implements IntersectionObserver {
   readonly root: Element | null = null
   readonly rootMargin: string = ''
+  readonly scrollMargin: string = ''
   readonly thresholds: ReadonlyArray<number> = []
   observe = () => {}
   unobserve = () => {}
