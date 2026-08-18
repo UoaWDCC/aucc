47a4e4f69b3b092810d888902530c97dc8e23297
yrska <jonathanw15382@gmail.com>
2026-08-18 12:36:40 +1200

    chore: add payload migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260724_005630_add_merch_global_and_intro_text from './20260724_005630_add_merch_global_and_intro_text'
+import * as migration_20260818_003440 from './20260818_003440'
 
 export const migrations = [
   {
     up: migration_20260724_005630_add_merch_global_and_intro_text.up,
     down: migration_20260724_005630_add_merch_global_and_intro_text.down,
     name: '20260724_005630_add_merch_global_and_intro_text',
+  },
+  {
+    up: migration_20260818_003440.up,
+    down: migration_20260818_003440.down,
+    name: '20260818_003440',
   },
 ]
